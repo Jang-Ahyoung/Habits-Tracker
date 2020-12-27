2b7a21feeab5b6e62cdd15706d75cb4e8c98f451
Jang-Ahyoung <wegotthelove27@gmail.com>
2020-12-27 21:46:30 +0900

    👷‍♀️ modify detail css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() {
         return (
-            <nav>
+            <nav className='navbar'>
                 <i className ="navbar-logo fas fa-leaf"></i>
-                <span>Habit Tracker</span>
+                <span >Habit Tracker</span>
                 {/* //props로 전달받은 totalCount 전달해줘! -> app.js에서 navbar에 실제로 전달해줘야해!!! */}
                 <span className ="navbar-count">{this.props.totalCount}</span>
             </nav>
